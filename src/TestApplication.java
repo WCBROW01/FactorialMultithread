7708fb9bae3c83245e800a0b37136f7abe81cd97
Will Brown <kyrosewoodgang@gmail.com>
2021-02-19 09:23:03 -0500

    Add command line arguments.

--- src/TestApplication.java
+++ src/TestApplication.java
@@ -9,14 +9,38 @@ import java.util.Scanner;
 
 public class TestApplication {
 	
+	private static int number, threadCount;
+	
 	/**
-	 * Main method. Mostly for entering parameters and displaying text.
+	 * Main method. Handles parameters.
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		if (args.length == 0 || args[0].equals("-i") || args[0].equals("--interactive")) {
+			interactive();
+		} else if (args[0].equals("-h") || args[0].equals("--help")) {
+			printHelp();
+		} else {
+			threadCount = 0; // default in case no value is entered
+			
+			// Iterate through arguments
+			for (int argument = 0; argument < args.length; argument++) {
+				if (args[argument].equals("-n") || args[argument].equals("--number")) {
+					number = Integer.parseInt(args[argument + 1]);
+				} else if (args[argument].equals("-t") || args[argument].equals("--threads")) {
+					threadCount = Integer.parseInt(args[argument] + 1);
+				}
+			}
+			
+			printFactorial();
+		}
+	}
+	
+	/**
+	 * Interactive portion of the program. Runs if you enter no arguments or enter "-i" or "--interactive".
+	 */
+	private static void interactive() {
 		Scanner input = new Scanner(System.in);
-		Factorial myFactorial;
-		int number, threadCount;
 		char again = 'y';
 		
 		while (again == 'y' || again == 'Y') {
@@ -30,8 +54,7 @@ public class TestApplication {
 				System.out.print("Enter number to complete factorial: ");
 				number = input.nextInt();
 				
-				myFactorial = new Factorial(number, threadCount);
-				System.out.println("Factorial of " + number + " is " + myFactorial.getResult());
+				printFactorial();
 			
 			} catch (InputMismatchException e) {
 				System.out.println("Invalid number!");
@@ -47,4 +70,19 @@ public class TestApplication {
 		input.close();
 	}
 	
+	/**
+	 * Creates a Factorial and prints it to the console.
+	 */
+	private static void printFactorial() {
+		Factorial myFactorial = new Factorial(number, threadCount);
+		System.out.println("Factorial of " + number + " is " + myFactorial.getResult());
+	}
+	
+	/**
+	 * Prints help information.
+	 */
+	private static void printHelp() {
+		// TODO: Create help text
+		System.out.println("Unimplemented feature: help");
+	}
 }
